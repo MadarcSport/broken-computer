4186735eea74dec82adda902b27b8ec9eead34ca
MadarcSport <marctoubia1985@gmail.com>
2026-05-14 17:08:10 +0200

    Fix R3F JSX typing for points to unblock build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -19,6 +19,8 @@ declare module "react" {
       pointLight: any;
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
       hemisphereLight: any;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      points: any;
     }
   }
 }
